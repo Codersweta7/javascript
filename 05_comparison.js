0d11bf7b7592753da074d40f1ad58808de0e3e7f
sweta pandey <sweta803pandey@gmail.com>
2024-09-02 18:00:54 +0530

    comparison

---
+++ 05_comparison.js
@@ -0,0 +1,9 @@
+//while comparing ensure data type is same
+
+console.log(null>0);
+console.log(null==0);//equality check 
+console.log(null>=0);//comparison operator=>converts null to a nummerical value i.e, 0.
+// equality check and comparison operator act diffrently . 
+// in case of undefine we get false values
+console.log("2"===2);
+//in this case checking 1)is this string ? 2) value .
