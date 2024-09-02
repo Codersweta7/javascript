44250f95030a55e64c0f6720fc8f3e9d4595b442
sweta pandey <sweta803pandey@gmail.com>
2024-09-02 14:40:19 +0530

    conversion

---
+++ 03_conversion.js
@@ -0,0 +1,22 @@
+// let score = "50abc";
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+//  console.log(valueInNumber);//NaN
+// // when input score is null we get 0;
+// // whem inp ut score is undefine we get NaN
+// // when input is bollean such as t/f we get its respective value 1 or 0;
+// // when we input string we again get nan;
+// // when we input 1 we get true.
+// let isloggedin = 1;
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+ let value=35;
+ let stringvalue = String(value);
+ console.log(value);
+ console.log(typeof stringvalue);
